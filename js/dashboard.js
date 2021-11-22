d25bc7491f1f9aacae7f15080dd2a4045d1b5890
Min Khant Maw <mr.minkhantmaw@gmail.com>
2021-11-23 00:21:28 +0630

    second-chart

--- js/dashboard.js
+++ js/dashboard.js
@@ -71,3 +71,61 @@ const ov = new Chart(ctx, {
     },
   },
 });
+
+let orderFromPlace=[5,15,4,9,7];
+let places=["ဘိုကလေး","ဖျာပုံ","အုတ်ကျွတ်","ကမကလူ","ကဇောင်းမ"];
+const ct = document.getElementById('op').getContext('2d');
+const op = new Chart(ct, {
+    type: 'doughnut',
+    data: {
+        labels: places,
+        datasets: [{
+            label: '# of Votes',
+            data: orderFromPlace,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            display: false,
+          },
+        ],
+        xAxes: [
+          {
+            display: false,
+            gridLines: [
+              {
+                display: false,
+              },
+            ],
+          },
+        ],
+      },
+      legend: {
+        display: true,
+        position: "top",
+        labels: {
+          fontColor: "#333",
+          usePointStyle: true,
+        },
+      },
+    },
+});
